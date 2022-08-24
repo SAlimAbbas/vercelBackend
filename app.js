78e1c45cc5a045a4ff405a387e71bf8c14abf922
SAlimAbbas <alimabbas44891@gmail.com>
2022-08-24 19:13:57 +0530

    temp

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ const auth=require("./middleware/auth")
 // const cookieParser = require("cookie-parser")
 
 const app=express();
-app.use(cors());
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json())
-// app.use(cookieParser())
+app.use(cors());
 
 app.get("/",(req,res)=>{
     res.end("home route")
